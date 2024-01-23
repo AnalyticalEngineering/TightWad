a1aaa08f3a8d5db9838a0f232786deb4cba23164
Joseph DeWeese <jdeweese@weldtascics.systems>
2024-01-22 18:57:50 -0600

    Initial Commit

---
+++ TightWad/TightWadApp.swift
@@ -0,0 +1,17 @@
+//
+//  TightWadApp.swift
+//  TightWad
+//
+//  Created by J. DeWeese on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct TightWadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
